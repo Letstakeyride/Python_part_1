c94ea7c043b6d695e0afd15a359a240e1ebdcc98
Mochinut <waka332@Mail.ru>
2023-04-25 16:01:27 +0300

    tdd

--- main.py
+++ main.py
@@ -1,16 +1,81 @@
-# This is a sample Python script.
+def test_greeting():
+    """
+    Напишите программу, которая выводит на экран приветствие.
+    """
+    name = "Анна"
+    age = 25
+    # TODO Сформируйте нужную строку
+    output = ""
+    # Проверяем результат
+    assert output == "Привет, Анна! Тебе 25 лет."
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def test_rectangle():
+    """
+    Напишите программу, которая берет длину и ширину прямоугольника
+    и считает его периметр и площадь.
+    """
+    a = 10
+    b = 20
+
+    # TODO сосчитайте периметр
+    perimeter = 0
+    assert perimeter == 60
+
+    # TODO сосчитайте площадь
+    area = 0
+    assert area == 200
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def test_circle():
+    """
+    Напишите программу, которая берет радиус круга и выводит на экран его длину и площадь.
+    Используйте константу PI
+    """
+    r = 23
+    # TODO сосчитайте площадь
+    area = 0
+    assert area == 1661.9025137490005
+
+    # TODO сосчитайте длину окружности
+    length = 0
+    assert length == 144.51326206513048
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def test_random_list():
+    """
+    Создайте список из 10 случайных чисел от 1 до 100 и отсортируйте его по возрастанию.
+    """
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # TODO создайте список
+    l = []
+    assert len(l) == 10
+    assert l[0] < l[-1]
+
+
+def test_unique_elements():
+    """
+    Удалите из списка все повторяющиеся элементы
+    """
+    l = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10]
+    # TODO удалите повторяющиеся элементы
+
+    assert isinstance(l, list)
+    assert len(l) == 10
+    assert l == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def test_dicts():
+    """
+    Создайте словарь из двух списков.
+    Используйте первый список как ключи, а второй - как значения.
+    Выведите на экран все значения словаря.
+    Подсказка: используй встроенную функцию zip.
+    """
+    first = ["a", "b", "c", "d", "e"]
+    second = [1, 2, 3, 4, 5]
+    # TODO создайте словарь
+    d = {}
+
+    assert isinstance(d, dict)
+    assert len(d) == 5
